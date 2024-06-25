8dd124161593e5112ac132f3978ed42d598a456b
Francescabattistini <166622198+Francescabattistini@users.noreply.github.com>
2024-06-25 16:30:05 +0200

    finito

--- javaD7.js
+++ javaD7.js
@@ -101,18 +101,197 @@ document.querySelector("footer").addEventListener("click", function () {
         Crea una funzione che crei una tabella nell'elemento con id "tableArea". 
         La tabella avrà 5 elementi e questa struttura: immagine, nome prodotto, quantità, prezzo
      */
+const generateTable = function () {
+  const tableArea = document.getElementById("tableArea");
+  const table = document.createElement("table");
+  table.style.border = "1px solid black";
+  const thead = document.createElement("thead");
+  const colonnaHeading1 = document.createElement("th");
+  colonnaHeading1.innerText = "immagine";
+  const colonnaHeading2 = document.createElement("th");
+  colonnaHeading2.innerText = "nome prodotto";
+  const colonnaHeading3 = document.createElement("th");
+  colonnaHeading3.innerText = "quantità";
+  const colonnaHeading4 = document.createElement("th");
+  colonnaHeading4.innerText = "prezzo";
+  tableArea.appendChild(table);
+  table.appendChild(thead);
+  thead.appendChild(colonnaHeading1);
+  thead.appendChild(colonnaHeading2);
+  thead.appendChild(colonnaHeading3);
+  thead.appendChild(colonnaHeading4);
+  for (let i = 0; i < 5; i++) {
+    const row = document.createElement("tr");
+    table.appendChild(row);
+  }
+  const rows = document.querySelectorAll("table tr");
+  for (let b = 0; b < rows.length; b++) {
+    for (let i = 0; i < 4; i++) {
+      const td = document.createElement("td");
+      rows[b].appendChild(td);
+    }
+  }
+  const td = document.querySelectorAll("tr > td");
+  for (let i = 0; i < td.length; i += 4) {
+    let randomSrc = `https://picsum.photos/100?random=${i}`;
+    const element = td[i];
+    const img = document.createElement("img");
+    img.src = randomSrc;
+    img.style.width = "100%";
+    element.appendChild(img);
+  }
+  for (let i = 1; i < td.length; i += 4) {
+    const element = td[i];
+    const randomNames = [
+      "dog",
+      "potato",
+      "cow",
+      "parrot",
+      "stone",
+      "watch",
+      "house",
+      "biolumonesance",
+      "brake",
+      "potato/salad",
+      "trip",
+      "hipopotomus",
+      "mask",
+      "chicken",
+      "Pneumonoultramicroscopicsilicovolcanoconiosis",
+      "dog",
+      "potato",
+      "cow",
+      "parrot",
+      "stone",
+      "watch",
+      "house",
+      "biolumonesance",
+      "brake",
+      "potato/salad",
+      "trip",
+      "hipopotomus",
+      "mask",
+      "chicken",
+    ];
+    element.innerText = randomNames[i];
+  }
+  for (let i = 2; i < td.length; i += 4) {
+    const element = td[i];
+    const randomNumbers = [];
+    for (let i = 0; i < td.length; i++) {
+      const randomNumber = Math.floor(Math.random() * 10);
+      randomNumbers.push(randomNumber);
+    }
+    element.innerText = randomNumbers[i];
+  }
+  for (let i = 3; i < td.length; i += 4) {
+    const element = td[i];
+    const randomPrices = [];
+    for (let i = 0; i < td.length; i++) {
+      const randomNumber = Math.floor(Math.random() * 20);
+      const randomNumberPrice = parseInt(randomNumber) + ".00$";
+      randomPrices.push(randomNumberPrice);
+    }
+    element.innerText = randomPrices[i];
+  }
+  table.style.width = "50%";
+  table.style.textAlign = "center";
+  const border = document.querySelectorAll("table td");
+  const border1 = document.querySelectorAll("table th");
+  for (let i = 0; i < border.length; i++) {
+    const element = border[i];
+    element.style.border = "1px solid black";
+  }
+};
 
+generateTable();
 /* ESERCIZIO 12
         Crea una funzione che aggiunga una riga alla tabella precedentemente creata e fornisca i dati necessari come parametri
      */
 
-const addRow = function () {};
+const addRow = function () {
+  const table = document.getElementsByTagName("table")[0];
+  const row1 = document.createElement("tr");
+  table.appendChild(row1);
+  const rows = document.querySelectorAll("table tr");
+  for (let i = 0; i < 4; i++) {
+    const td = document.createElement("td");
+    rows[rows.length - 1].appendChild(td);
+  }
+  const td = document.querySelectorAll("tr > td");
+  let randomSrc = `https://picsum.photos/100`;
+  const imgSlot = td[td.length - 4];
+  const img = document.createElement("img");
+  img.src = randomSrc;
+  img.style.width = "100%";
+  imgSlot.appendChild(img);
+  const nameSlot = td[td.length - 3];
+  const randomNames = [
+    "dog",
+    "potato",
+    "cow",
+    "parrot",
+    "stone",
+    "watch",
+    "house",
+    "biolumonesance",
+    "brake",
+    "potato/salad",
+    "trip",
+    "hipopotomus",
+    "mask",
+    "chicken",
+    "Pneumonoultramicroscopicsilicovolcanoconiosis",
+    "dog",
+    "potato",
+    "cow",
+    "parrot",
+    "stone",
+    "watch",
+    "house",
+    "biolumonesance",
+    "brake",
+    "potato/salad",
+    "trip",
+    "hipopotomus",
+    "mask",
+    "chicken",
+  ];
+  nameSlot.innerText = randomNames[td.length - 3];
+  const quantitySlot = td[td.length - 2];
+  const randomNumbers = [];
+  const randomquantity = Math.floor(Math.random() * 10);
+  randomNumbers.push(randomquantity);
+  quantitySlot.innerText = randomNumbers[0];
+  const priceSlot = td[td.length - 1];
+  const randomPrices = [];
+  const randomNumber = Math.floor(Math.random() * 20);
+  const randomNumberPrice = parseInt(randomNumber) + ".00$";
+  randomPrices.push(randomNumberPrice);
+  priceSlot.innerText = randomPrices[0];
+  const border = document.querySelectorAll("table td");
+  const border1 = document.querySelectorAll("table th");
+  for (let i = 0; i < border.length; i++) {
+    const element = border[i];
+    element.style.border = "1px solid black";
+  }
+};
+
+addRow();
 
 /* ESERCIZIO 14
        Crea una funzione che nasconda le immagini della tabella quando eseguita
      */
+/* 
+const hideAllImages = function () {
+  const images = document.querySelectorAll("table img");
+  for (let i = 0; i < images.length; i++) {
+    const element = images[i];
+    element.style.opacity = "0";
+  }
+};
 
-const hideAllImages = function () {};
+hideAllImages(); */
 
 /* EXTRA ESERCIZIO 15
        Crea una funzione che cambi il colore del h2 con id "changeMyColor" con un colore random ad ogni click ricevuto
@@ -120,7 +299,7 @@ const hideAllImages = function () {};
 const changeColorWithRandom = function () {
   // Funzione per generare un colore random in formato esadecimale
   function getRandomColor() {
-    return "#" + Math.floor(Math.random() * 2550214).toString(16);
+    return "#" + Math.floor(Math.random() * 12457845).toString(16);
   }
 
   // Seleziona l'elemento h2 con id "changeMyColor"
